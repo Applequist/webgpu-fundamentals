f01a8081f4256d419a0833c5032a886c9770ad9c
Brieuc Desoutter <brieuc.desoutter@gmail.com>
2024-04-30 11:47:49 +0300

    Empty winit window creation.

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use winit::{
+    application::ApplicationHandler,
+    event::WindowEvent,
+    event_loop::{ControlFlow, EventLoop},
+    window::Window,
+};
+
+#[derive(Default)]
+struct App {
+    window: Option<Window>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+        self.window = Some(
+            event_loop
+                .create_window(Window::default_attributes())
+                .unwrap(),
+        );
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &winit::event_loop::ActiveEventLoop,
+        window_id: winit::window::WindowId,
+        event: winit::event::WindowEvent,
+    ) {
+        match event {
+            WindowEvent::CloseRequested => {
+                println!("The close button was pressed; stopping...");
+                event_loop.exit();
+            }
+            _ => (),
+        }
+    }
+}
+
+pub fn run() {
+    env_logger::init();
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.set_control_flow(ControlFlow::Poll);
+    let mut app = App::default();
+    let _ = event_loop.run_app(&mut app);
+}
+
+fn main() {
+    run();
+}
